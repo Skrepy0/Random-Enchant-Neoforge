eaa84ddbb7d4541fe7fee0ab6b1a9fd913465d5c
Skrepy <703189721@qq.com>
2025-12-26 01:00:45 +0800

    🔖添加物品标签

--- src/main/java/com/random_enchant/datagen/ModItemTagsProvider.java
+++ src/main/java/com/random_enchant/datagen/ModItemTagsProvider.java
@@ -1,9 +1,14 @@
 package com.random_enchant.datagen;
 
 import com.random_enchant.RandomEnchant;
+import com.random_enchant.item.ModItemTags;
+import com.random_enchant.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagBuilder;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +22,30 @@ public class ModItemTagsProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-
+        tag(ModItemTags.FURY_OF_FLY_AVAILABLE)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ItemTags.SWORDS)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ItemTags.DURABILITY_ENCHANTABLE)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ModItemTags.WIND_BURST_AVAILABLE)
+                .addTag(ItemTags.MACE_ENCHANTABLE)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ModItemTags.QUICK_CHARGE_AVAILABLE)
+                .addTag(ItemTags.CROSSBOW_ENCHANTABLE)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ModItemTags.CHANNELING)
+                .add(ModItems.PEARL_SPEAR.get())
+                .addTag(ItemTags.TRIDENT_ENCHANTABLE);
+        tag(ItemTags.SWORD_ENCHANTABLE)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ItemTags.WEAPON_ENCHANTABLE)
+                .add(ModItems.PEARL_SPEAR.get());
+        tag(ModItemTags.FROST_WALKER)
+                .add(Items.ARROW)
+                .addTag(ItemTags.FOOT_ARMOR_ENCHANTABLE);
+        tag(ModItemTags.CHANNELING)
+                .add(ModItems.PEARL_SPEAR.get())
+                .addTag(ItemTags.TRIDENT_ENCHANTABLE);
     }
 }
--- src/main/java/com/random_enchant/item/ModItemTags.java
+++ src/main/java/com/random_enchant/item/ModItemTags.java
@@ -1,4 +1,22 @@
 package com.random_enchant.item;
 
+import com.random_enchant.RandomEnchant;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
 public class ModItemTags {
+    public static final TagKey<Item> FURY_OF_FLY_AVAILABLE = of("fury_of_fly_available");
+    public static final TagKey<Item> QUICK_CHARGE_AVAILABLE = of("quick_charge_available");
+    public static final TagKey<Item> WIND_BURST_AVAILABLE = of("wind_burst_available");
+    public static final TagKey<Item> CHANNELING = of("channeling_available");
+    public static final TagKey<Item> FROST_WALKER = of("frost_walker_available");
+    public static TagKey<Item> of(String id){
+        return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(RandomEnchant.MOD_ID,id));
+    }
+    public static void registerModItemTags(){
+        RandomEnchant.LOGGER.info("Register Mod Item Tags");
+    }
 }
