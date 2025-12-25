36bf2df835e63a3328b757494053da41b56950c8
Skrepy <703189721@qq.com>
2025-12-26 01:01:39 +0800

    ➕添加自定义谓词

--- src/main/java/com/random_enchant/item/ModItemModelProperties.java
+++ src/main/java/com/random_enchant/item/ModItemModelProperties.java
@@ -1,4 +1,21 @@
 package com.random_enchant.item;
 
+import com.random_enchant.RandomEnchant;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+
 public class ModItemModelProperties {
+    public static void registerProperties() {
+        ItemProperties.register(
+                ModItems.PEARL_SPEAR.asItem(),
+                ResourceLocation.fromNamespaceAndPath(RandomEnchant.MOD_ID, "cooldown"),
+                (stack, world, entity, seed) -> {
+                    if (entity instanceof Player player) {
+                        return player.getCooldowns().isOnCooldown(stack.getItem()) ? 1.0F : 0.0F;
+                    }
+                    return 0.0F;
+                }
+        );
+    }
 }
