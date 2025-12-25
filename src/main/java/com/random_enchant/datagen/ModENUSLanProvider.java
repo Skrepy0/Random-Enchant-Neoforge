799a615c6c616394a9396288b1707c8c11348828
Skrepy <703189721@qq.com>
2025-12-26 00:57:45 +0800

    📈更新语言文件

--- src/main/java/com/random_enchant/datagen/ModENUSLanProvider.java
+++ src/main/java/com/random_enchant/datagen/ModENUSLanProvider.java
@@ -9,9 +9,42 @@ public class ModENUSLanProvider extends LanguageProvider {
     public ModENUSLanProvider(PackOutput output) {
         super(output, RandomEnchant.MOD_ID, "en_us");
     }
+    public static String toRoman(int number) {
+        if (number < 11 || number > 255) {
+            throw new IllegalArgumentException("输入必须在11到255之间");
+        }
 
+        // 定义罗马数字的基本组成部分
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        // 分解数字的各个部分
+        int thousandPart = number / 1000;
+        int hundredPart = (number % 1000) / 100;
+        int tenPart = (number % 100) / 10;
+        int unitPart = number % 10;
+
+        // 构建罗马数字字符串
+        return thousands[thousandPart] +
+                hundreds[hundredPart] +
+                tens[tenPart] +
+                units[unitPart];
+    }
     @Override
     protected void addTranslations() {
-        add(ModItems.PEARL_SPEAR.get(), "pearl_spear");
+        add("itemGroup.random_enchant.title", "Random Enchant");
+        add(ModItems.PEARL_SPEAR.get(), "Pearl Spear");
+
+        add("enchantment.random_enchant.fury_of_fly","§aFury Of Fly");
+
+        add("item.tooltip.random_enchant.for_shift_tooltip","Press §6[SHIFT]§r show detail information");
+        add("item.tooltip.random_enchant.pearl_spear.detail_description_1","§bLeft-click to attack the entity and teleport, right-click to teleport§r");
+        add("item.tooltip.random_enchant.pearl_spear.detail_description_2","§bEntities in the teleportation path take 8 base damage(real damage is related to the level of §a[Sweep Edge]§b)§r");
+        add("item.tooltip.random_enchant.pearl_spear.detail_description_3","§bThe greater the relative speed to the target when left-clicking to attack,the higher the damage§r");
+        for (int i = 11 ; i < 256;i++){
+            add("enchantment.level."+i,toRoman(i));
+        }
     }
 }
--- src/main/java/com/random_enchant/datagen/ModZHCNLangProvider.java
+++ src/main/java/com/random_enchant/datagen/ModZHCNLangProvider.java
@@ -12,6 +12,16 @@ public class ModZHCNLangProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
+        add("itemGroup.random_enchant.title", "随机附魔");
         add(ModItems.PEARL_SPEAR.get(), "珍珠矛");
+
+        add("enchantment.random_enchant.fury_of_fly","§aFly之怒");
+
+        add("item.tooltip.random_enchant.for_shift_tooltip","按下§6[SHIFT]§r查看详细信息");
+        add("item.tooltip.random_enchant.pearl_spear.detail_description_1","§b左键攻击实体并瞬移，右键瞬移§r");
+        add("item.tooltip.random_enchant.pearl_spear.detail_description_2","§b瞬移路径上的实体会受到8点基础伤害(实际伤害与§a[横扫之刃]§b等级有关)§r");
+        add("item.tooltip.random_enchant.pearl_spear.detail_description_3","§b左键攻击时与目标的相对速度越大，伤害越高§r");
+
+
     }
 }
