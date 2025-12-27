3eff1128c6a6a725e9e7c2c845180eb8628aecf9
Skrepy <703189721@qq.com>
2025-12-27 11:03:02 +0800

    ➕添加附魔标签数据生成

---
+++ src/main/java/com/random_enchant/datagen/ModEnchantmentTagsProvider.java
@@ -0,0 +1,24 @@
+package com.random_enchant.datagen;
+
+import com.random_enchant.RandomEnchant;
+import com.random_enchant.enchantment.ModEnchantmentTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.EnchantmentTagsProvider;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModEnchantmentTagsProvider extends EnchantmentTagsProvider {
+    public ModEnchantmentTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, RandomEnchant.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(ModEnchantmentTags.FURY_OF_FLY_EXCLUSIVE)
+                .add(Enchantments.CHANNELING);
+    }
+}
